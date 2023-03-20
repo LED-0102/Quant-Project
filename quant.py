50659b41f23dc37f832f54b07754b2c48260bb68
LED-0102 <117679837+LED-0102@users.noreply.github.com>
2023-03-20 08:19:57 +0530

    Add files via upload

---
+++ quant.py
@@ -0,0 +1,11 @@
+import pandas
+import numpy
+
+led = pandas.read_csv('stocks_data.csv')
+
+led = led.loc[(led['p/e']<40) &(led['cagr']>=10.0) & (led['eps_2022']-led['eps_2021']>0) & (led['eps_2021']-led['eps_2020']>0)]
+led['criteria'] = led['cagr']/led['p/e']
+led = led.sort_values(by='criteria', ascending=False)
+led.index = numpy.arange(1, len(led)+1)
+led.to_csv('preferred_stocks.csv')
+
